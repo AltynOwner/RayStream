dd5171b835b16829bb2b7560d4a6d32023b28d0d
AltynOwner <altyn.owner@gmail.com>
2026-08-03 20:20:22 -0700

    Implement service worker registration
    Added service worker registration to enhance app functionality.

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const connectButton = document.getElementById("connectButton");
 connectButton.addEventListener("click", () => {
     window.open("https://google.com", "_blank");
 });
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then(() => console.log("Service Worker Registered"))
+            .catch(console.error);
+    });
+}
